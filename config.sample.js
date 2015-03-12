4b810f7d4adb2946aab591e84db80c3bd8aa2daa
jysperm <jysperm@gmail.com>
2015-03-12 21:30:03 +0800

    Configure file

---
+++ config.sample.js
@@ -0,0 +1,7 @@
+module.exports = {
+  port: 14117,
+
+  GITHUB_CLIENT_ID: 'Client-ID',
+  GITHUB_CLIENT_SECRET: 'Client-Secret',
+  GITHUB_CALLBACK_URL: 'http://cocona.newsbee.io/auth/github/callback'
+};
