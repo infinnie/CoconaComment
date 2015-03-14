6f754c9c883a6f5394464a24c55290e951ff8a27
jysperm <jysperm@gmail.com>
2015-03-14 23:52:36 +0800

    API Design

--- app.js
+++ app.js
@@ -62,6 +62,66 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use(passport.initialize());
 app.use(passport.session());
 
+/*
+  GET /comments?domain=jysperm.me&topic=unit-of-abstract
+
+  [{
+    "_id": "548c10bf5727a30e1890b4d0",
+    "domain": "jysperm.me",
+    "topic": "unit-of-abstract",
+    "created_at": "2015-03-14T15:38:06.224Z",
+    "parent_id": null,
+    "body": "正因为 Python 在提供复杂抽象手段的同时，又提出了一些限制，才能被广泛地使用。"
+    "author": {
+      "_id": "548c10bf5727a30e1890b4d0",
+      "id": 1191561,
+      "provider": "github",
+      "profile": {
+        "login": "jysperm",
+        "name": "王子亭",
+        "avatar_url": "https://avatars.githubusercontent.com/u/1191561?v=3",
+        "blog": "https://jysperm.me",
+        "location": "Suzhou, Jiangsu, China",
+        "followers": 275
+      }
+    }
+  }, {
+    // ...
+  }]
+*/
+app.get('/comments', function(req, res) {
+  // TODO
+});
+
+/*
+  POST /comments HTTP/1.1
+  Content-Type: application/json
+  Cookie: required
+
+  {
+    "domain": "jysperm.me",
+    "topic": "unit-of-abstract",
+    "parent_id": "548c10bf5727a30e1890b4d0", // optional
+    "body": "很多人是没有系统地学习过「抽象」这项技能的，对于他们而言，编程语言必须直接提供强有力的抽象手段。"
+  }
+
+  HTTP/1.1 200 OK
+  Content-Type: application/json
+
+  {
+    "_id": "548c10bf5727a30e1890b4d0",
+    "domain": "jysperm.me",
+    "topic": "unit-of-abstract",
+    "created_at": "2015-03-14T15:38:06.224Z",
+    "parent_id": "548c10bf5727a30e1890b4d0",
+    "body": "很多人是没有系统地学习过「抽象」这项技能的，对于他们而言，编程语言必须直接提供强有力的抽象手段。",
+    "author": // ...
+  }
+*/
+app.post('/comments', ensureAuthenticated, function(req, res) {
+  // TODO
+});
+
 app.get('/auth/github', passport.authenticate('github'));
 
 app.get('/auth/github/callback', passport.authenticate('github'), function(req, res) {
