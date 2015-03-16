1590003ad0118ccfab7dc2515079067742f4ec4d
infinnie <w4ergf@qq.com>
2015-03-16 21:16:17 +0800

    UI design draft added

--- public/cocona.js
+++ public/cocona.js
@@ -1,1 +1,27 @@
-console.log('Hello, world.');
+var count = 0;
+jQuery(function ($) {
+    var prevHeight = 0, running = false, calcHeight = function (ts, dur) {
+        ts = +ts || +new Date();
+        dur = dur || 400;
+        if (+new Date() - ts > dur) {
+            count++;
+            return;
+        }
+        prevHeight = $("html").height();
+        parent.postMessage(prevHeight + 10, "*");
+        // setTimeout(calcHeight, 50, ts);
+        if ("requestAnimationFrame" in window) {
+            requestAnimationFrame(function () {
+                calcHeight(ts, dur);
+            });
+        } else {
+            setTimeout(function () { calcHeight(ts, dur); }, 30);
+        }
+    };
+    $("#editor").on("focus blur", function () {
+        calcHeight();
+    }).on("keyup", function () {
+        calcHeight(null, 1)
+    });
+    calcHeight(+new Date(), 1);
+});
