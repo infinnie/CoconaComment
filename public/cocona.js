971922ccda4a9b298fa4c8e56867c5712f0ffbd5
infinnie <w4ergf@qq.com>
2015-03-19 22:16:22 +0800

    Repair updates to client-side script

--- public/cocona.js
+++ public/cocona.js
@@ -1,27 +1,23 @@
-var count = 0;
-jQuery(function ($) {
-    var prevHeight = 0, running = false, calcHeight = function (ts, dur) {
-        ts = +ts || +new Date();
-        dur = dur || 400;
-        if (+new Date() - ts > dur) {
-            count++;
-            return;
-        }
-        prevHeight = $("html").height();
-        parent.postMessage(prevHeight + 10, "*");
-        // setTimeout(calcHeight, 50, ts);
-        if ("requestAnimationFrame" in window) {
-            requestAnimationFrame(function () {
-                calcHeight(ts, dur);
-            });
-        } else {
-            setTimeout(function () { calcHeight(ts, dur); }, 30);
+var Cocona = (function () {
+    "use strict";
+    var frames = {};
+    if (typeof window.addEventListener === "function") {
+        window.addEventListener("message", function (e) {
+            frames[e.source.name].style.height = e.data + "px";
+        }, false);
+    } else {
+        window.onmessage = function (e) {
+            frames[e.source.name].style.height = e.data + "px";
+        };
+    }
+    return {
+        init: function (id, theme) {
+            var elem = document.getElementById(id), ifr = document.createElement("iframe"), frameName = ("CoconaComment" + Math.random()).replace(/\W/g, "");
+            if (!elem) { return; }
+            ifr.name = frameName;
+            ifr.src = "/index.html" + (theme ? "?theme=" + theme : "");
+            frames[frameName] = ifr;
+            elem.appendChild(ifr);
         }
     };
-    $("#editor").on("focus blur", function () {
-        calcHeight();
-    }).on("keyup", function () {
-        calcHeight(null, 1)
-    });
-    calcHeight(+new Date(), 1);
-});
+}());
