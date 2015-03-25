f83f690890a2bf3cc60fa8c5f996eccca0074934
infinnie <w4ergf@qq.com>
2015-03-25 22:19:17 +0800

    Modify rich text editor

--- public/cocona-frame.js
+++ public/cocona-frame.js
@@ -2,8 +2,8 @@
 /// <reference path="../bower_components/jquery/jquery.js"/>
 (function ($, ko) {
     "use strict";
-    var count = 0, comments = window.comments = ko.observableArray([]);
-    ko.applyBindings(comments);
+    var count = 0, App = { comments: ko.observableArray([]) };
+    ko.applyBindings(App);
 
     $(function () {
         var prevHeight = 0, link = $("<link>").attr("rel", "stylesheet"), calcHeight = function (ts, dur) {
@@ -24,6 +24,94 @@
             } else {
                 setTimeout(function () { calcHeight(ts, dur); }, 30);
             }
+        }, cleanDOM = function (element) {
+            /// <param name="element" type="HTMLElement"/>
+            // TODO: hyperlinks
+            element = $(element);
+            element.find("font,b b,i i,a a").each(function () {
+                this.outerHTML = this.innerHTML;
+            });
+            element.find("i b").each(function () {
+                var range = document.createRange(), parent = $(this).parents("i").get(0), frag, i;
+                range.selectNodeContents(parent);
+                range.setStartBefore(this);
+                frag = range.extractContents();
+                $(this).html("<i>" + $(this).html() + "</i>");
+                range.setStartAfter(parent);
+                range.setEndAfter(parent);
+                range.insertNode(this);
+                range.setStartAfter(this);
+                i = document.createElement("i");
+                i.appendChild(frag);
+                range.insertNode(i);
+            });
+            element.find("b,i").filter(":empty").remove();
+            element.find("b").each(function () {
+                var sib = this.nextSibling, range, frag;
+                if (!sib) { return; }
+                if (sib.nodeType === 1 && sib.tagName.toLowerCase() === "b") {
+                    range = document.createRange();
+                    range.selectNodeContents(sib);
+                    frag = range.extractContents();
+                    $(sib).remove();
+                    this.appendChild(frag);
+                }
+            });
+            element.find("i").each(function () {
+                var sib = this.nextSibling, range, frag;
+                if (sib.nodeType === 1 && sib.tagName.toLowerCase() === "i") {
+                    range = document.createRange();
+                    range.selectNodeContents(sib);
+                    frag = range.extractContents();
+                    $(sib).remove();
+                    this.appendChild(frag);
+                }
+            });
+        }, insertParagraphOnRange = function (range, refNode) {
+            /// <param name="range" type="Range"/>
+            /// <param name="refNode" type="HTMLElement"/>
+            /// <var name="blockTags">Represents the block tags that may occur in the editor.</var>
+            var blockTags = "div,p,blockquote,pre,h1,h2,h3,h4,h5,h6", startContainer, container, frag, p;
+
+            startContainer = range.startContainer;
+            if (startContainer.nodeType === 3) {
+                startContainer = startContainer.parentNode;
+            }
+            // Use the parents() method--which I would rather be called ancestors()--because there may be multiple levels
+            // between the inline container element, and the block ancestor element.
+            startContainer = $(startContainer).is(blockTags) ? startContainer : $(startContainer).parents(blockTags).get(0);
+
+            container = range.endContainer;
+            if (container.nodeType === 3) {
+                container = container.parentNode;
+            }
+            container = $(container).is(blockTags) ? container : $(container).parents(blockTags).get(0);
+            range.deleteContents();
+
+            if (startContainer === container) {
+                range.setEnd(container, container.childNodes.length);
+                frag = range.extractContents();
+                p = document.createElement("p");
+                p.appendChild(frag);
+            } else {
+                p = container;
+                container = startContainer;
+            }
+            if (container === refNode) {
+                container = $(container).html("<p>" + $(container).html() + "</p>").find("p").get(0);
+            }
+            if (!$(p).text() && !$(p).find("br").length) {
+                $(p).append("<br>");
+            }
+            cleanDOM(container);
+            cleanDOM(p);
+            range.setStartAfter(container);
+            range.insertNode(p);
+            return {
+                range: range,
+                paragraph: p,
+                container: container
+            };
         };
 
         if (/[?&]theme=dark(?:&|$)/i.test(location.search)) {
@@ -63,32 +151,13 @@
         }).on("blur", "[role=textbox]", function () {
             $("#editorBox").find("[data-command]").removeClass("active");
         }).on("keypress", "[role=textbox]", function (e) {
-            var selection, container, range, frag, p, done, that = this;
+            var selection, range, p, done, that = this;
             if (e.keyCode === 13 && (!e.shiftKey) && (!e.ctrlKey)) {
                 try {
                     selection = window.getSelection();
                     range = selection.getRangeAt(0);
-                    range.deleteContents();
-                    container = range.endContainer;
-                    if (container.nodeType === 3) {
-                        container = container.parentNode;
-                    }
-                    container = $(container).is("div,p,blockquote,pre") ? container : $(container).parent("div,p,blockquote,pre").get(0);
-                    range.setEnd(container, container.childNodes.length);
-                    frag = range.extractContents();
-                    p = document.createElement("p");
-                    p.appendChild(frag);
-                    if (container === that) {
-                        container = $(container).html("<p>" + $(container).html() + "</p>").find("p").get(0);
-                    }
-                    if (!$(p).text() && !$(p).find("br").length) {
-                        $(p).append("<br>");
-                    }
-                    $(p).find("font").each(function () {
-                        this.outerHTML = this.innerHTML;
-                    });
-                    range.setStartAfter(container);
-                    range.insertNode(p);
+
+                    p = insertParagraphOnRange(range, that).paragraph;
                     while (p.firstChild && p.firstChild.nodeType === 1 && p.firstChild.nodeName.toLowerCase() !== "br") {
                         p = p.firstChild;
                     }
@@ -118,7 +187,7 @@
             return !done;
         }).on("paste", "[role=textbox]", function (e) {
             e = e.originalEvent;
-            var cd = e.clipboardData || window.clipboardData, that = this, done, text, hyperLink, selection, startContainer, container, range, isDiff, textArray, textNode, frag, p;
+            var cd = e.clipboardData || window.clipboardData, that = this, done, text, hyperLink, selection, insertResult, container, range, textArray, textNode, frag, p;
             // IE TP has no clipboardData for the event, yet
             // but does everything else like Chrome.
             if (cd) {
@@ -128,6 +197,7 @@
                     text = cd.getData("text");
                 }
                 if (text) {
+                    text = $.trim(text);
                     if (/^(?:https?|ftp|mailto)\:\S+$/.test(text)) {
                         // TODO: insert hyperlink
                         hyperLink = document.createElement("a");
@@ -153,42 +223,12 @@
                             selection = window.getSelection();
                             range = selection.getRangeAt(0);
 
-                            startContainer = range.startContainer;
-                            if (startContainer.nodeType === 3) {
-                                startContainer = startContainer.parentNode;
-                            }
-                            startContainer = $(startContainer).is("div,p,blockquote,pre") ? startContainer : $(startContainer).parent("div,p,blockquote,pre").get(0);
+                            // first insert a paragraph, as if the enter were pressed
+                            insertResult = insertParagraphOnRange(range, that);
+                            container = insertResult.container;
+                            p = insertResult.paragraph;
 
-                            container = range.endContainer;
-                            if (container.nodeType === 3) {
-                                container = container.parentNode;
-                            }
-                            container = $(container).is("div,p,blockquote,pre") ? container : $(container).parent("div,p,blockquote,pre").get(0);
-
-                            range.deleteContents();
-
-                            if (startContainer === container) {
-                                range.setEnd(container, container.childNodes.length);
-                                frag = range.extractContents();
-                                p = document.createElement("p");
-                                p.appendChild(frag);
-                            } else {
-                                p = container;
-                                container = startContainer;
-                            }
-                            if (container === that) {
-                                container = $(container).html("<p>" + $(container).html() + "</p>").find("p").get(0);
-                            }
-                            if (!$(p).text() && !$(p).find("br").length) {
-                                $(p).append("<br>");
-                            }
-                            $(p).find("font").each(function () {
-                                this.outerHTML = this.innerHTML;
-                            });
-                            range.setStartAfter(container);
-                            range.insertNode(p);
-
-                            textArray = text.replace(/^[\r\n]+/, "").replace(/[\r\n]+$/, "").split(/[\r\n]+/);
+                            textArray = text.split(/[\r\n]+/);
                             frag = document.createDocumentFragment();
                             container.appendChild(document.createTextNode(textArray.shift() || ""));
                             if (textArray.length) {
@@ -220,7 +260,7 @@
                             // }
                         } catch (x2) {
                             try {
-                                document.selection.createRange().pasteHTML(text.replace(/^[\r\n]+/, "").replace(/[\r\n]+$/, "").replace(/[\r\n]+/g, "<\/p><p>"));
+                                document.selection.createRange().pasteHTML(text.replace(/[\r\n]+/g, "<\/p><p>"));
                                 done = true;
                             } catch (x4) { }
                         }
@@ -231,15 +271,14 @@
         }).on("submit", function () {
             return false;
         });
-        $.getJSON("/comments/?" + (+new Date()), function (data) {
+        $.getJSON(/*"/comments/?"*/"/format.json?" + (+new Date()), function (data) {
             $.each(data, function () {
                 // console.log(this);
-                comments.push(this);
+                App.comments.push(this);
+                setTimeout(function () {
+                    calcHeight(+new Date(), 1);
+                }, 400);
             });
         });
-
-        setTimeout(function () {
-            calcHeight(+new Date(), 1);
-        }, 400);
     });
 }(window.jQuery, window.ko));
